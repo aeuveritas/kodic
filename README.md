9c659efb1ea105c0e6d3edd76463d76057f2e886
Karl Jeong <aeuveritas@gmail.com>
2020-09-17 00:39:17 +0900

    isolate functionality into methods

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # kodic
+Notify Korean meaning of English word from Naver dictionary
+
+# TODO
+- [ ] webserver
--- main.go
+++ main.go
@@ -6,79 +6,149 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/atotto/clipboard"
 	"github.com/gen2brain/beeep"
 )
 
+// Mean holds word mean
 type Mean struct {
 	Value string `json:"value,omitempty"`
 }
 
+// MeansCollector holds means
 type MeansCollector struct {
 	Means []Mean `json:"means,omitempty"`
 }
 
+// Item holds meansCollectors
 type Item struct {
 	MeansCollectors []MeansCollector `json:"meansCollector,omitempty"`
 }
 
+// Word holds items
 type Word struct {
 	Items []Item `json:"items,omitempty"`
 }
 
+// SearchResultListMap holds word
 type SearchResultListMap struct {
 	Word Word `json:"WORD,omitempty"`
 }
 
+// SearchResultMap holds search result list map
 type SearchResultMap struct {
 	SearchResultListMap SearchResultListMap `json:"searchResultListMap,omitempty"`
 }
 
-type NaverDictResponse struct {
+// DictResponse holds search result map
+type DictResponse struct {
 	SearchResultMap SearchResultMap `json:"searchResultMap,omitempty"`
 }
 
+const (
+	interval = time.Second
+	rootURL  = "https://en.dict.naver.com/api3/enko/"
+)
+
+var (
+	previousWord string
+	regCode      *regexp.Regexp
+)
+
+func validateWord(word string) string {
+	word = strings.Trim(word, " ")
+	if word == previousWord {
+		return ""
+	}
+	previousWord = word
+
+	if !regCode.MatchString(word) {
+		log.Println("wrong word: " + word)
+		return ""
+	}
+
+	return word
+}
+
+func searchWord(word string) []byte {
+	searchURL := fmt.Sprintf(rootURL+"search?m=\"pc\"&query=\"%s\"", word)
+	resp, err := http.Get(searchURL)
+	if err != nil {
+		log.Println("HTTP request error: " + err.Error())
+		return nil
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("failed to read response: " + err.Error())
+		return nil
+	}
+
+	return body
+}
+
+func parseResponse(body []byte) *DictResponse {
+	var dictResponse DictResponse
+	err := json.Unmarshal(body, &dictResponse)
+	if err != nil {
+		log.Println("failed to unmarshal: " + err.Error())
+		return nil
+	}
+	if len(dictResponse.SearchResultMap.SearchResultListMap.Word.Items) == 0 {
+		log.Println("empty result")
+		return nil
+	}
+
+	return &dictResponse
+}
+
+func sendNotification(word string, dictResponse *DictResponse) {
+	means := dictResponse.SearchResultMap.SearchResultListMap.Word.Items[0].MeansCollectors[0].Means
+
+	var text string
+	for _, mean := range means {
+		text += mean.Value + " "
+	}
+
+	beeep.Notify("Word: "+word, "Mean: "+text, "./icon.jpg")
+}
+
+func run() {
+	defer time.Sleep(interval)
+
+	word, err := clipboard.ReadAll()
+	if err != nil {
+		log.Println("cannot read clipboard : " + err.Error())
+		return
+	}
+
+	word = validateWord(word)
+	if word == "" {
+		return
+	}
+
+	body := searchWord(word)
+	if body == nil {
+		return
+	}
+
+	dictResponse := parseResponse(body)
+	if dictResponse == nil {
+		return
+	}
+
+	sendNotification(word, dictResponse)
+}
+
 func main() {
-	oldWord := ""
+	previousWord = ""
+	regCode, _ = regexp.Compile("^[a-zA-Z]+$")
 	for {
-		defer time.Sleep(500 * time.Microsecond)
-
-		newWord, err := clipboard.ReadAll()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if newWord == oldWord {
-			continue
-		}
-		oldWord = newWord
-
-		url := fmt.Sprintf("https://en.dict.naver.com/api3/enko/search?m=\"pc\"&query=\"%s\"", newWord)
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-
-		body, readErr := ioutil.ReadAll(resp.Body)
-		if readErr != nil {
-			log.Fatal(readErr)
-		}
-
-		var ndr NaverDictResponse
-		jsonErr := json.Unmarshal(body, &ndr)
-		if jsonErr != nil {
-			log.Fatal(jsonErr)
-		}
-		means := ndr.SearchResultMap.SearchResultListMap.Word.Items[0].MeansCollectors[0].Means
-
-		var text string
-		for _, mean := range means {
-			text += mean.Value + " "
-		}
-
-		go beeep.Notify("Word: "+newWord, "Mean: "+text, "")
+		run()
 	}
 }
